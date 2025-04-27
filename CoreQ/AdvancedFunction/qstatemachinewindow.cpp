9477136e1794cadbafcf459b2f0d0d79c50722da
ewq <312>
2025-04-27 16:09:15 +0800

    1.增加现代C++文件夹。 2.增加状态机窗口。

---
+++ CoreQ/AdvancedFunction/qstatemachinewindow.cpp
@@ -0,0 +1,10 @@
+#include "qstatemachinewindow.h"
+
+//State Machine Examples
+//The Event Transitions Example 说明了QEventTransition怎么在状态机使用。把QEvent继承到状态机框架中使用。
+//Factorial 说明QSignalTransition、QFinalState、QState怎么使用
+//
+
+QStateMachineWindow::QStateMachineWindow(QWidget *parent)
+    : QWidget{parent}
+{}
---
+++ CoreQ/AdvancedFunction/qstatemachinewindow.h
@@ -0,0 +1,15 @@
+#ifndef QSTATEMACHINEWINDOW_H
+#define QSTATEMACHINEWINDOW_H
+
+#include <QWidget>
+
+class QStateMachineWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit QStateMachineWindow(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // QSTATEMACHINEWINDOW_H
